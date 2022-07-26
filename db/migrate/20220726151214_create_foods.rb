494a24d0576309da072e751e8a30d2e51399a1d2
Henry Nawrocki <hdev@sztanski.com>
2022-07-26 10:13:41 -0500

    Add Food model, controller, views and migrations

---
+++ db/migrate/20220726151214_create_foods.rb
@@ -0,0 +1,13 @@
+class CreateFoods < ActiveRecord::Migration[7.0]
+  def change
+    create_table :foods do |t|
+      t.string :name
+      t.string :unit
+      t.decimal :price
+      t.integer :quantity
+      t.belongs_to :users
+
+      t.timestamps
+    end
+  end
+end
