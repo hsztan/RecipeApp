5083ef70ce5e051e2900ffef27ec5a02c77c31b3
Henry Nawrocki <hdev@sztanski.com>
2022-07-26 14:19:46 -0500

    Create PublicRecipe route, controller action and view & functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  root 'foods#index'
   resources :recipes
   resources :foods
   devise_for :users
-  root 'foods#index'
 
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get '/public_recipes' => 'recipes#public_recipes'
   # root "articles#index"
 end
